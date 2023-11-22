222fec63a4d2437e93cd95d032e525f3a36b0e9d
Astra <abhaypratapsinghwd@gmail.com>
2023-11-22 13:51:37 +0530

    UI for Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,34 @@
 import React, { useContext } from 'react'
 import { WeatherContext } from '../Context';
 import { Navigate, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
-  const {cityName,setCityName,fetchCurrentWeather,fetchWeatherForecast} = useContext(WeatherContext);
+  const { cityName, setCityName, fetchCurrentWeather, fetchWeatherForecast } = useContext(WeatherContext);
   const navigate = useNavigate();
-  const handleFormSubmit = (e) =>{
+  const handleFormSubmit = (e) => {
     e.preventDefault();
     fetchCurrentWeather();
     fetchWeatherForecast();
     navigate('/weather');
   }
+  const handleResetCityName = () =>{
+    setCityName("");
+  }
   return (
-    <div>
-      <h2>Home Page</h2>
-      <form onSubmit={handleFormSubmit}>
-          <input
-            type="text"
-            value={cityName}
-            onChange={(e) => setCityName(e.target.value)}
-            placeholder='Enter City Name:'
-          />
-        <button type="submit">Get Weather</button>
+    <div className='bg-black min-h-screen text-2xl relative'>
+      <h1 className='text-sky-500 font-bold text-4xl py-4 px-8'>Cloudcast</h1>
+      <form onSubmit={handleFormSubmit} className='searchCity relative '>
+        <button type="submit"><FontAwesomeIcon icon={faSearch} className='absolute top-4 left-4 text-sky-400 hover:text-sky-500' /></button>
+        <input
+          type="text"
+          value={cityName}
+          onChange={(e) => setCityName(e.target.value)}
+          placeholder='Enter City Name:'
+          className='px-12 rounded-lg focus:outline-none hover:cursor-pointer h-14 shadow-md shadow-slate-800'
+        />
+        <FontAwesomeIcon icon={faCircleXmark} className='absolute top-4 right-4 text-sky-300 cursor-pointer hover:text-sky-500' onClick={handleResetCityName}/>
       </form>
     </div>
   )
